1260c981b3659eb8e70ac338368196af9d6e5928
bloosky <blooskydiver@gmail.com>
2020-07-01 22:08:07 -0400

    initial commit

---
+++ src/main/java/com/github/mqhub/forecastr/ForecastrApplication.java
@@ -0,0 +1,13 @@
+package com.github.mqhub.forecastr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastrApplication.class, args);
+	}
+
+}
